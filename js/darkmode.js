e4a78de5d8c2dc6641defb93aa8f2b19255a4f20
Dorian VOYDIE <dorian.voydie@gmail.com>
2022-03-29 20:29:47 +0200

    Derniere version Lightmode. Mise à jour : Darkmode by default + tout le site change (sauf footer et resume)

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,11 +1,12 @@
 function toggle_darkmode() {
   document.getElementById("darkmode-toggler").classList.toggle("fa-sun");
+  document.getElementById("darkmode-toggler").classList.toggle("fa-moon");
   console.log("Button clicked");
   if (
-    document.getElementById("darkmode-toggler").classList.contains("fa-sun")
+    document.getElementById("darkmode-toggler").classList.contains("fa-moon")
   ) {
-    document.body.classList.add("dark");
+    document.body.classList.add("light");
   } else {
-    document.body.classList.remove("dark");
+    document.body.classList.remove("light");
   }
 }
