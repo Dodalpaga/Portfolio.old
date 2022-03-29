3511214722063effd08c590aa5697ec6caaf7edb
Dorian VOYDIE <dorian.voydie@gmail.com>
2022-03-29 20:11:54 +0200

    MaJ minime

--- js/about.js
+++ js/about.js
@@ -1,34 +1,13 @@
-'use strict';
+"use strict";
 
-function about_in_animations()
-{
-	let done = false;
-	let elements = document.querySelectorAll('#about_section .in_animation');
-	let section = document.querySelector('#about_section .photo');
+function about_in_animations() {
+  window.addEventListener("scroll", (e) => {
+    in_animation_check();
+  });
 
-	async function in_animation_check()
-	{
-		if (!done && is_in_viewport(section))
-		{
-			for (let i of elements)
-			{
-				i.style.clipPath = 'circle(49.7%)';
-				await sleep(300);
-			}
+  window.addEventListener("resize", (e) => {
+    in_animation_check();
+  });
 
-			done = true;
-		}
-	}
-
-	window.addEventListener('scroll', (e) =>
-	{
-		in_animation_check();
-	});
-
-	window.addEventListener('resize', (e) =>
-	{
-		in_animation_check();
-	});
-
-	in_animation_check();
+  in_animation_check();
 }
